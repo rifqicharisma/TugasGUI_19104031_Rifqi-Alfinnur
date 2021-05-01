a90f43f61fbe59acd5189fbc4d8eb50c9da26fa5
Rifqi Charisma <rifqibarker@gmail.com>
2021-05-01 08:43:11 +0700

    modul4

---
+++ Praktikum/Modul4/DemoQTDesigner.py
@@ -0,0 +1,20 @@
+import sys
+from masukkan import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+class DemoQtDesigner(QDialog):
+    def __init__(self,parent = None):
+        QDialog.__init__(self,parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.ui.hallo.clicked.connect(self.halloClicked)
+
+    def halloClicked(self):
+        QMessageBox.information(self, 'Demo Qt Designer', 'Hallo %s, apa kabar?' % self.ui.nameEdit.text())
+
+if __name__ == "__main__":
+    a = QApplication(sys.argv)
+    form = DemoQtDesigner()
+    form.show()
+    a.exec_()
